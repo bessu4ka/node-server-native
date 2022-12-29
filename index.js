6803e1ffde2ff6ca4b1c6178a7ea45194a5befa8
Roman Volkov <bessukabit@gmail.com>
2022-12-29 09:16:24 +0200

    add express

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req, res) => {
-  res.send('Hello');
-});
+app.use('/users', userRouter);
 
 app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
+  console.log(`Сервер запущен на http://localhost:${port}`);
 });
