4b859c55772ee06bc5eb4185163efba18f1ca3df
Roman Volkov <bessukabit@gmail.com>
2023-01-03 10:02:21 +0200

    add eslint & prettier & settings

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,20 +10,20 @@ import 'reflect-metadata';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
-  constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
-    super(loggerService);
-    this.bindRoutes([
-      { path: '/register', method: 'post', func: this.register },
-      { path: '/login', method: 'post', func: this.login },
-    ]);
-  }
+	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+		super(loggerService);
+		this.bindRoutes([
+			{ path: '/register', method: 'post', func: this.register },
+			{ path: '/login', method: 'post', func: this.login },
+		]);
+	}
 
-  login(req: Request, res: Response, next: NextFunction) {
-    // this.ok(res, 'login');
-    next(new HTTPError(401, 'authorization error', 'login'));
-  }
+	login(req: Request, res: Response, next: NextFunction): void {
+		// this.ok(res, 'login');
+		next(new HTTPError(401, 'authorization error', 'login'));
+	}
 
-  register(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, 'register');
-  }
+	register(req: Request, res: Response, next: NextFunction): void {
+		this.ok(res, 'register');
+	}
 }
